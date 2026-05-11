06c6f95c0dc2a055545fd57f66b30d74c0cb11c0
anawer121 <592526420@qq.com>
2026-05-12 00:57:04 +0800

    增加交换两个数的功能

--- test.c
+++ test.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 
+void Swap(int *num1,int *num2)
+{
+    int number;
+
+    number=*num1;
+    *num1=*num2;
+    *num2=number;
+
+}
 int main()
 {
     printf("hello world");
+
     
     return 0;
 }   
